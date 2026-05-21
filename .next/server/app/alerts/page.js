9fc14c07f4886ef59c560f1140048706c2206069
piusuloma <piusuloma@gmail.com>
2026-05-21 18:09:43 +0100

    My first commit

--- .next/server/app/alerts/page.js
+++ .next/server/app/alerts/page.js
@@ -396,7 +396,7 @@ module.exports = require("util");
 var __webpack_require__ = require("../../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/next","vendor-chunks/sonner","vendor-chunks/lucide-react","vendor-chunks/@swc"], () => (__webpack_exec__("(rsc)/./node_modules/next/dist/build/webpack/loaders/next-app-loader/index.js?name=app%2Falerts%2Fpage&page=%2Falerts%2Fpage&appPaths=%2Falerts%2Fpage&pagePath=private-next-app-dir%2Falerts%2Fpage.tsx&appDir=C%3A%5CUsers%5CUSER%5CDesktop%5CPOS%5Chaven11-nextjs%5Capp&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&rootDir=C%3A%5CUsers%5CUSER%5CDesktop%5CPOS%5Chaven11-nextjs&isDev=true&tsconfigPath=tsconfig.json&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=&middlewareConfig=e30%3D&isGlobalNotFoundEnabled=!")));
+var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/next","vendor-chunks/@swc","vendor-chunks/sonner","vendor-chunks/lucide-react"], () => (__webpack_exec__("(rsc)/./node_modules/next/dist/build/webpack/loaders/next-app-loader/index.js?name=app%2Falerts%2Fpage&page=%2Falerts%2Fpage&appPaths=%2Falerts%2Fpage&pagePath=private-next-app-dir%2Falerts%2Fpage.tsx&appDir=C%3A%5CUsers%5CUSER%5CDesktop%5CPOS%5Chaven11-nextjs%5Capp&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&rootDir=C%3A%5CUsers%5CUSER%5CDesktop%5CPOS%5Chaven11-nextjs&isDev=true&tsconfigPath=tsconfig.json&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=&middlewareConfig=e30%3D&isGlobalNotFoundEnabled=!")));
 module.exports = __webpack_exports__;
 
 })();
